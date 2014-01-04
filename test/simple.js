c1f54ba4d6f04227e1c707014728b3beac08781c
Earl Celis <copongcopong@gmail.com>
2014-01-05 01:46:26 +0800

    partials test

--- test/simple.js
+++ test/simple.js
@@ -129,10 +129,61 @@ test('using config - data added via callback using ractivore.beforeCreate() ', f
     
 });
 
-test('simple boot dir with partial', function (t) {
+test('using addTemplate and setData to setup', function (t) {
+		
+		var view = ractivore();
+		var data = {
+			"header": "header",
+			"content": "content"
+		};
+		
+		//not good approach though. async library to the rescue!
+		view.addTemplate(__dirname + '/views/one/template.html')
+			.then(function(){
+				view.setData(data).then(function(){
+					view.create().then(function(){
+						var html = view.rhtml.toHTML();
+			
+						t.ok((html.indexOf('<h1>header</h1>') > -1), "header found");
+						t.ok((html.indexOf('<p>content</p>') > -1), "content found");
+						t.end();
+					});
+				});
+		
+			});
+		
+    
+});
+
+test('simple boot dir with partial file "footer.part.html"', function (t) {
 		
 		var view = ractivore();
 		var config = {dir: __dirname + '/views/one-with-partial'}
+		view.create(config).then(function(){
+			t.ok(view.partials.footer, "footer in views.partials");
+			
+			var html = view.rhtml.toHTML();
+				
+			t.ok((html.indexOf('<footer>end</footer>') > -1), "footer partial rendered");
+			
+			t.end();
+		});
+    
+});
+
+test('load partials at config', function (t) {
+		var dt = require(__dirname + '/views/one-with-partial/data.json');
+		console.log(dt);
+		var view = ractivore();
+		var config = {
+			template: __dirname + '/views/one-with-partial/template.html',
+			data: dt,
+			partials: {
+				footer:  __dirname + '/views/one-with-partial/footer.part.html'
+			}
+			
+		};
+		
 		view.create(config).then(function(){
 			t.ok(view.partials.footer, "footer in views.partials");
 			
