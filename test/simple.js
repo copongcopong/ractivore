7467f48eee69f4620d8cb1d848ab3306b00bdbda
Earl Celis <copongcopong@gmail.com>
2014-01-04 20:54:10 +0800

    added tape tests;

---
+++ test/simple.js
@@ -0,0 +1,57 @@
+var test = require('tape');
+var ractivore = require('../index.js');
+
+test('simple boot dir', function (t) {
+		
+		var view = ractivore();
+		var config = {dir: __dirname + '/views/one'}
+		view.create(config).then(function(){
+			var html = view.rhtml.toHTML();
+			
+			t.ok((html.indexOf('<h1>one</h1>') > -1), "header found");
+			t.ok((html.indexOf('<p>content</p>') > -1), "content found");
+			t.end();
+		});
+    
+});
+
+test('simple boot dir with partial', function (t) {
+		
+		var view = ractivore();
+		var config = {dir: __dirname + '/views/one-with-partial'}
+		view.create(config).then(function(){
+			t.ok(view.partials.footer, "footer in views.partials");
+			
+			var html = view.rhtml.toHTML();
+				
+			t.ok((html.indexOf('<footer>end</footer>') > -1), "footer partial rendered");
+			
+			t.end();
+		});
+    
+});
+
+test('subview rendering', function (t) {
+		
+		var view = ractivore();
+		var config = {dir: __dirname + '/views/with-subs/base'};
+		
+		config.subs = {
+			subview: {
+				dir: __dirname + '/views/with-subs/sub'
+			}
+		}
+		
+		view.create(config).then(function(){
+			t.ok(view.subs.subview, "subview in views.subs");
+			t.ok(view.subs.subview.isSub, "views.subs.isSub is true");
+			
+			//console.log(view.subs.subview);
+			var html = view.rhtml.toHTML();
+				
+			t.ok((html.indexOf('<em>subcontent</em>') > -1), "subview html rendered");
+			
+			t.end();
+		});
+    
+});
