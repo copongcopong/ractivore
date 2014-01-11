9c722fada058b0cbfe38f9f666ac5beae73b22af
Earl Celis <copongcopong@gmail.com>
2014-01-11 09:08:39 +0800

    added options for parser; added guid maker;

--- index.js
+++ index.js
@@ -7,5 +7,8 @@ var rview = new view({}, ractive, ractivoreTpl);
 exports = module.exports = function(options) {
 	rview.startTimer();
 	rview.initOptions(options);
+	if(options && options.parserOpts !== undefined) {
+		rview.tpls.setParserOptions(options.parserOpts);
+	}
 	return rview;
 }
